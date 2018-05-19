78f77121efb89c947268b7014d96891129a79be8
AidarSh <aidar32010@gmail.com>
2018-05-19 18:58:05 +0400

    123

--- js/script.js
+++
@@ -1,8 +0,0 @@
-menu.onclick = function myFunction() {
-  var x = document.getElementById("mynavmenu");
-  if(x.className === "navmenu"){
-    x.className += " responsive";
-  }else {
-    x.className = "navmenu";
-  }
-}
